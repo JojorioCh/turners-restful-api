d5bca7cb2be40a9654546d8a887d2a7ada2c8e14
JojorioCh <josephch@missionreadyhq.com>
2023-05-25 00:47:12 +1200

    just a few cosmetic code changes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import env from 'dotenv'
-import app from './app'
+import env from 'dotenv';
+import app from './app';
 
-env.config()
+env.config();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-})
+	console.log(`Server started on port ${PORT}`);
+});
