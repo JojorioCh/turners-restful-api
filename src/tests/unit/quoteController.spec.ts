a62ac2fb9bc9fd650aa9f84f2792113944aa188b
Aubrey Mina <aubreybmina@gmail.com>
2023-05-24 18:36:31 +1200

    added quoteController test

---
+++ src/tests/unit/quoteController.spec.ts
@@ -0,0 +1,25 @@
+import * as quoteController from '../../controller/quoteApiControl'
+import { createRequest, createResponse } from 'node-mocks-http'
+
+describe('getAllCars', () => {
+  test('should send all car via response', () => {
+    // Arrange
+    const req = createRequest()
+    const res = createResponse()
+    const expected = [
+      {
+        id: 1,
+        carValue: 6613,
+        riskRating: 5,
+        annualPremium: 330.65,
+        monthlyPremium: 27.55,
+      },
+    ]
+
+    // Act
+    quoteController.getAllQuotes(req, res)
+
+    //Assert
+    expect(res.json()._getData()).toEqual(expected)
+  })
+})
